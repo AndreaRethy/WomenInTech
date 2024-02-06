2e2ffdd65d5b634781ab5f87ec178b60bf2e7fdb
AlbaVidalEsteve <66486832+AlbaVidalEsteve@users.noreply.github.com>
2024-02-06 12:48:37 +0100

    modal js
    working
    needs css

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,27 @@
 // Get the modal
-let modal = document.querySelector('#myModal');
+let modal = document.querySelector('#modal');
+console.log(modal);
 // let content = document.querySelector('.modal-content');
 
 // Get the button that opens the modal
-let btn = document.querySelector('#myBtn');
+let abrir = document.querySelector('#abrir');
 
 // Get the <span> element that closes the modal
-let span = document.querySelector('.close')[0];
+let cerrar = document.querySelector('#cerrar');
 
-// When the user clicks on the button, open the modal
-document.querySelector('#myBtn').addEventListener('click', () =>{
-    modal.style.display = 'block';
+//Funcion abrir modal
+abrir.addEventListener('click', () =>{
+    modal.classList.remove('oculta-modal');
+    console.log('click en abrir');
 });
 
-// When the user clicks on <span> (x), close the modal
-document.querySelector('.close').addEventListener('click', () =>{
-    modal.style.display = 'none';
+//Funcion cerrar modal
+cerrar.addEventListener('click', () =>{
+    modal.classList.add('oculta-modal');
 });
-
-// span.onclick = function() {
-//   modal.style.display = "none";
-// //   content.style.display = "none";
-// }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    // content.style.display = "none";
-  }
-}
+//Segunda funcion cerrar modal haciendo click fuera
+window.addEventListener('click', (e)=>{
+    if (e.target == modal){
+        modal.classList.add('oculta-modal');
+    }
+});
