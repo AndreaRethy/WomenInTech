462b51e656659cc93e42bc5f04e9b423afcd0373
Andrea <125359387+AndreaRethy@users.noreply.github.com>
2024-02-06 19:11:42 +0100

    Toggle '.actual' on cards

--- js/scripts.js
+++ js/scripts.js
@@ -15,33 +15,49 @@ const btnRight = document.querySelector('#btn-right');
 SLIDER.insertAdjacentElement('afterbegin', sliderSectionLast);
 
 
-// Función listener para el botón derecho
+// Función listener para el botón izquierdo
 btnLeft.addEventListener('click', () =>{
     let sliderSection = document.querySelectorAll('.card');
     let sliderSectionLast = sliderSection[sliderSection.length-1];
+    let sliderActual = sliderSection[0];
+
+    sliderSection.forEach(slider => {
+        slider.classList.remove('actual');
+    })
+    sliderActual.classList.add('actual');
+    // console.log(sliderActual.innerHTML);
     
     SLIDER.style.marginLeft = '0';
     SLIDER.style.transition = 'all 0.5s';
     setTimeout(() =>{
         SLIDER.style.transition = 'none';
         SLIDER.insertAdjacentElement('afterbegin', sliderSectionLast);
-        SLIDER.style.marginLeft = '-65%';
+        SLIDER.style.marginLeft = '-100%';
     }, 500);
  });
 
-// Función listener para el botón izquierdo
+
+// Función listener para el botón derecho
 const MOVERIGHT = () =>{
     let sliderSectionFirst = document.querySelectorAll('.card')[0];
-    SLIDER.style.marginLeft = '-130%';
+    let sliderActual = sliderSection[1];
+
+    sliderSection.forEach(slider => {
+        slider.classList.remove('actual');
+    })
+    sliderActual.classList.add('actual');
+    // console.log(sliderActual.innerHTML);
+
+    SLIDER.style.marginLeft = '-200%';
     SLIDER.style.transition = 'all 0.5s';
     setTimeout(() =>{
         SLIDER.style.transition = 'none';
         SLIDER.insertAdjacentElement('beforeend', sliderSectionFirst);
-        SLIDER.style.marginLeft = '-65%';
+        SLIDER.style.marginLeft = '-100%';
     }, 500);
 };
-
 btnRight.addEventListener('click', MOVERIGHT);
+
 
 // Automatización
 setInterval(MOVERIGHT, 50000);
