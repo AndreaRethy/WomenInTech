c4829d01a93be8a8f2d79837d741ea096ec2d5b4
Andrea <125359387+AndreaRethy@users.noreply.github.com>
2024-02-06 12:06:22 +0100

    Add carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,47 @@
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".card");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+// Declaración de variables
+const SLIDER = document.querySelector('#slider');
+let sliderSection = document.querySelectorAll('.card');
 
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
+// Seleccionamos el últiomo slider
+let sliderSectionLast = sliderSection[sliderSection.length-1];
 
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-});
+
+//Declaramos variables para los botones
+const btnLeft = document.querySelector('#btn-left');
+const btnRight = document.querySelector('#btn-right');
+
+
+// Situamos la última imagen en la primera posición
+SLIDER.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+
+// Función listener para el botón derecho
+btnLeft.addEventListener('click', () =>{
+    let sliderSection = document.querySelectorAll('.card');
+    let sliderSectionLast = sliderSection[sliderSection.length-1];
+    
+    SLIDER.style.marginLeft = '0';
+    SLIDER.style.transition = 'all 0.5s';
+    setTimeout(() =>{
+        SLIDER.style.transition = 'none';
+        SLIDER.insertAdjacentElement('afterbegin', sliderSectionLast);
+        SLIDER.style.marginLeft = '-100%';
+    }, 500);
+ });
+
+// Función listener para el botón izquierdo
+const MOVERIGHT = () =>{
+    let sliderSectionFirst = document.querySelectorAll('.card')[0];
+    SLIDER.style.marginLeft = '-200%';
+    SLIDER.style.transition = 'all 0.5s';
+    setTimeout(() =>{
+        SLIDER.style.transition = 'none';
+        SLIDER.insertAdjacentElement('beforeend', sliderSectionFirst);
+        SLIDER.style.marginLeft = '-100%';
+    }, 500);
+};
+
+btnRight.addEventListener('click', MOVERIGHT);
+
+// Automatización
+setInterval(MOVERIGHT, 50000);
