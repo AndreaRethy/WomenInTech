3d2c92b3a26d8ece3043976ae65688d06b28be53
Andrea <125359387+AndreaRethy@users.noreply.github.com>
2024-02-07 11:02:09 +0100

    js update

--- js/scripts.js
+++ js/scripts.js
@@ -92,31 +92,3 @@ window.addEventListener('click', (e)=>{
         modal.classList.add('oculta-modal');
     }
 });
-
-//MODAL
-// Get the modal
-let modal = document.querySelector('#modal');
-// let content = document.querySelector('.modal-content');
-
-// Get the button that opens the modal
-let abrir = document.querySelector('#abrir');
-
-// Get the <span> element that closes the modal
-let cerrar = document.querySelector('#cerrar');
-
-//Funcion abrir modal
-abrir.addEventListener('click', () =>{
-    modal.classList.remove('oculta-modal');
-    console.log('click en abrir');
-});
-
-//Funcion cerrar modal
-cerrar.addEventListener('click', () =>{
-    modal.classList.add('oculta-modal');
-});
-//Segunda funcion cerrar modal haciendo click fuera
-window.addEventListener('click', (e)=>{
-    if (e.target == modal){
-        modal.classList.add('oculta-modal');
-    }
-});
